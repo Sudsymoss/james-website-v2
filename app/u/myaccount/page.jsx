eec994b17d253d3d0f7193b6e33db4eda328c61d
Sudsymoss <james1mowat@gmail.com>
2023-01-03 16:20:44 +1300

    account fixes

--- app/u/myaccount/page.jsx
+++ app/u/myaccount/page.jsx
@@ -9,7 +9,6 @@ const pb = new PocketBase(process.env.NEXT_PUBLIC_POCKETURL)
 
 
 export default function Account() {
-  const [formData, setFormData] = useState({})
   const notify = React.useCallback((type, message) => {
     toast({ type, message });
   }, []);
@@ -22,6 +21,9 @@ export default function Account() {
     fileInput.addEventListener('change', function () {
       for (let file of fileInput.files) {
         formData2.append('avatar', file);
+        document.getElementById("buttontop").style.backgroundColor = "#89b39f";
+        document.getElementById("uploadbutton").style.backgroundColor = "#1ee384";
+        document.getElementById("fileInputName").textContent = file.name;
       }
     });
   }, []);
@@ -29,16 +31,18 @@ export default function Account() {
 
   async function upload(e) {
     e.preventDefault()
+    document.getElementById("fileInputName").textContent = "Wait!";
+    document.getElementById("uploadbuttontext").textContent = "Uploading...";
     try {
       const record = await pb.collection('users').update(pb.authStore.model.id, formData2);
       notify("success", "Avatar updated!")
       console.log(record)
+      window.location.reload()
     } catch (error) {
       console.log(error)
       notify("error", "Failed to update avatar!")
       notify("info", "Maybe try removing your current one first?")
     }
-    window.location.reload()
   }
 
   async function rmAvatar(e) {
@@ -46,6 +50,7 @@ export default function Account() {
     try {
       const record = await pb.collection('users').update(pb.authStore.model.id, { "avatar": null });
       notify("success", "Avatar removed!")
+      window.location.reload()
     } catch (error) {
       console.log(error)
     }
@@ -76,31 +81,33 @@ export default function Account() {
           <p>Email: {pb.authStore.model.email}</p>
         </main>
         <div className={styles.forms}>
-          <form onSubmit={upload} className={styles.aform}>
+          <form onSubmit={upload} className={styles.aform} id="form">
             <h2>Upload avatar</h2>
             <img className={styles.usericon} src={pb.baseUrl + "/api/files/_pb_users_auth_/" + pb.authStore.model.id + "/" + pb.authStore.model.avatar || `https://github.com/identicons/${pb.authStore.model.username}.png`} />
             <button className={styles.button} onClick={rmAvatar} type="button" >
               <div class={styles.buttontop} id={styles.delavatar}>
                 <label class={styles.customfileupload}>
-                  <p id={styles.fileInputName}>Remove current</p>
+                  <p>Remove current</p>
                 </label>
               </div>
             </button>
+            <div className={styles.uploader}>
             <div className={styles.button}>
-              <div class={styles.buttontop}>
+              <div class={styles.buttontop} id="buttontop">
                 <label class={styles.customfileupload}>
                   <input type="file" name="file" id="fileInput" accept="image/*" className={styles.finput} />
-                  <p id={styles.fileInputName}>Select File</p>
+                  <p id="fileInputName">Select File</p>
                 </label>
               </div>
             </div>
             <button className={styles.button} type="submit">
-              <div class={styles.buttontop}>
+              <div class={styles.buttontop} id="uploadbutton">
                 <label class={styles.customfileupload}>
-                  <p id={styles.fileInputName}>Upload</p>
+                  <p id="uploadbuttontext">Upload</p>
                 </label>
               </div>
             </button>
+            </div>
           </form>
           <form className={styles.pform} onSubmit={delUser}>
             <h2>DELETE ACCOUNT</h2>
