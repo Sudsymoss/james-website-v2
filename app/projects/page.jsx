7f96198f91a2b37bdcc447c9fbc76cbbfe16fabb
Sudsymoss <103403655+Sudsymoss@users.noreply.github.com>
2023-01-03 19:27:13 +1300

    warning

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -2,9 +2,16 @@ import styles from './Projectspage.module.css'
 import { Suspense } from "react"
 import Load from '../loading'
 import Projects from './projects'
+
+import * as React from "react";
+import toast from "../../toast";
 export const revalidate = 1;
 // Pages are Server Components by default
 export default function Page() {
+  const notify = React.useCallback((type, message) => {
+    toast({ type, message });
+  }, []);
+  notify("warning", 'Projects may not update properly due to a server issue!')
   return (
     <div className={styles.main}>
                 <div className={styles.title}>
