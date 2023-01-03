9974d2b39d168039751bd0af8624477bdec7c406
Sudsymoss <103403655+Sudsymoss@users.noreply.github.com>
2023-01-03 19:15:12 +1300

    config cache?

--- next.config.js
+++ next.config.js
@@ -3,6 +3,18 @@ const nextConfig = {
     experimental: {
       appDir: true,
     },
+    headers: () => [
+      {
+        // Create glob to target specific pages you want
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store',
+          },
+        ],
+      },
+    ],
   }
   
   module.exports = nextConfig
