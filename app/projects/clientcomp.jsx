e97a4fa7f8d503ceb378014e56e39ad71db561e8
James M <103403655+Sudsymoss@users.noreply.github.com>
2023-03-02 12:12:54 +1300

    Update clientcomp.jsx

--- app/projects/clientcomp.jsx
+++ app/projects/clientcomp.jsx
@@ -13,10 +13,10 @@ export default function Page({children}) {
         {children}
       </div>
       <div className={styles.create}>
-          <Link href='/projects/create'>
+          <Link href='https://docs.suddsy.dev/' target='_blank'>
           <button className={styles.button}>
             <span className={styles.span}>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"></path><path fill="currentColor" d="M11 11V5h2v6h6v2h-6v6h-2v-6H5v-2z"></path></svg> Create
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"></path><path fill="currentColor" d="M11 11V5h2v6h6v2h-6v6h-2v-6H5v-2z"></path></svg> Docs
             </span>
           </button>
           </Link>
