644232df01ebd810ca80bd26293bb4bb49038f87
James M <103403655+Sudsymoss@users.noreply.github.com>
2023-03-08 09:57:19 +1300

    Update nav.jsx

--- app/nav.jsx
+++ app/nav.jsx
@@ -14,8 +14,6 @@ export default function Nav() {
                 <div className={styles.navdiv}>
                     <h3><Link legacyBehavior passHref href="/"><a>James M</a></Link></h3>
                     <ul className={styles.navitems}>
-                        //<li><Link legacyBehavior href="/" passHref><a>Home</a></Link></li>
-                        //<li><Link legacyBehavior href="/about" passHref><a>About</a></Link></li>
                         <li><Link legacyBehavior href="/projects" passHref><a>Projects</a></Link></li>
                         <li id="conflash"><Link legacyBehavior href="/auth/login" passHref ><a>Login</a></Link></li>
                     </ul>
@@ -28,8 +26,6 @@ export default function Nav() {
             <div className={styles.navdiv}>
                 <h3><Link passHref href="/" legacyBehavior><a>James M</a></Link></h3>
                 <ul className={styles.navitems}>
-                    //<li><Link href="/" passHref legacyBehavior><a>Home</a></Link></li>
-                    //<li><Link href="/about" passHref legacyBehavior><a>About</a></Link></li>
                     <li><Link href="/projects" passHref legacyBehavior><a>Projects</a></Link></li>
                     <li><Link href="/u/myaccount" passHref><img className={styles.usericon} src={pb.baseUrl + "/api/files/_pb_users_auth_/" + pb.authStore.model.id + "/" + pb.authStore.model.avatar ||`https://github.com/identicons/${pb.authStore.model.username}.png`}></img></Link></li>
                 </ul>
