35a7ccb3b16905e380c288da10f6ee42af174360
James M <103403655+Sudsymoss@users.noreply.github.com>
2023-03-08 01:26:04 +0000

    account requriement removed

--- app/nav.jsx
+++ app/nav.jsx
@@ -8,26 +8,12 @@ const pb = new PocketBase(process.env.NEXT_PUBLIC_POCKETURL)
 
 export default function Nav() {
     
-    if(pb.authStore.isValid == false){
-        return (
-            <div className={styles.container}>
-                <div className={styles.navdiv}>
-                    <h3><Link legacyBehavior passHref href="/"><a>James M</a></Link></h3>
-                    <ul className={styles.navitems}>
-                        <li><Link legacyBehavior href="/projects" passHref><a>Projects</a></Link></li>
-                        <li id="conflash"><Link legacyBehavior href="/auth/login" passHref ><a>Login</a></Link></li>
-                    </ul>
-                </div>
-            </div>
-        )
-    }
     return (
         <div className={styles.container}>
             <div className={styles.navdiv}>
-                <h3><Link passHref href="/" legacyBehavior><a>James M</a></Link></h3>
+                <h3><Link legacyBehavior passHref href="/"><a>James M</a></Link></h3>
                 <ul className={styles.navitems}>
-                    <li><Link href="/projects" passHref legacyBehavior><a>Projects</a></Link></li>
-                    <li><Link href="/u/myaccount" passHref><img className={styles.usericon} src={pb.baseUrl + "/api/files/_pb_users_auth_/" + pb.authStore.model.id + "/" + pb.authStore.model.avatar ||`https://github.com/identicons/${pb.authStore.model.username}.png`}></img></Link></li>
+                    <li><Link legacyBehavior href="/projects" passHref><a>Projects</a></Link></li>
                 </ul>
             </div>
         </div>
