aa895496c8304d65b88c6b6645de5e76c7cb80ac
Sudsymoss <james1mowat@gmail.com>
2023-03-19 14:37:24 +1300

    better

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,5 +1,4 @@
-export const dynamic = 'force-dynamic'
-export const revalidate = 0
+export const revalidate = 120
 import { Suspense } from "react"
 import Projects from './projects'
 import Clientcomp from './clientcomp'
