cebcb7fcccf0e8463024b5f47ec8618acda920bc
sudsymoss <james1mowat@gmail.com>
2023-04-03 16:37:34 +1200

    nav update & tab button

--- app/nav.jsx
+++ app/nav.jsx
@@ -7,15 +7,14 @@ import PocketBase from 'pocketbase';
 const pb = new PocketBase(process.env.NEXT_PUBLIC_POCKETURL)
 
 export default function Nav() {
-    
+
     return (
         <div className={styles.container}>
             <div className={styles.navdiv}>
                 <h3><Link legacyBehavior passHref href="/"><a>James M</a></Link></h3>
                 <ul className={styles.navitems}>
-                    <li><Link href="/projects"><button className={styles.pbutton}>
-    Projects
-</button></Link></li>
+                <li className={styles.normalnavitem}><Link href="https://gallery.suddsy.dev/" target='_blank' aria-label='photo gallery'>Gallery</Link></li>
+                    <li><Link href="/projects"><button className={styles.pbutton} aria-label='projects page link'>Projects</button></Link></li>
                 </ul>
             </div>
         </div>
