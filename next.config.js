b16d905eecacec8a7bb88a74d422022600571589
sudsymoss <james1mowat@gmail.com>
2023-04-11 09:31:03 +1200

    security

--- next.config.js
+++ next.config.js
@@ -1,9 +1,35 @@
-/** @type {import('next').NextConfig} */
+const securityHeaders = [
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'strict-origin-when-cross-origin'
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';"
+  },
+];
+
 const nextConfig = {
-    experimental: {
-      appDir: true,
-    },
-  }
-  
-  module.exports = nextConfig
-  
+  experimental: {
+    appDir: true,
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
