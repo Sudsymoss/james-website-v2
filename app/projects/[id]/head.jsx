7e1c31c052fcb4081b11a47d4b9751f05efef65e
sudsymoss <james1mowat@gmail.com>
2023-04-17 20:35:25 +1200

    googlebot

--- app/projects/[id]/head.jsx
+++ app/projects/[id]/head.jsx
@@ -3,8 +3,9 @@ export default function Head({params}) {
       <>
         <title>P: {params.id}</title>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
-        <meta name="description" content="James Mowat, Javascript devloper" />
+        <meta name="description" content="BOOO 👻" />
         <link rel="icon" href="/favicon.ico" />
+        <meta name="robots" content="noindex, max-snippet:0, noarchive, notranslate, noimageindex, unavailable_after: 2024-01-01"></meta>
       </>
     );
   }
--- app/projects/head.jsx
+++ app/projects/head.jsx
@@ -5,6 +5,7 @@ export default function Head() {
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <meta name="description" content="James Mowat, Javascript devloper" />
         <link rel="icon" href="/favicon.ico" />
+        <meta name="robots" content="noindex, max-snippet:0, noarchive, notranslate, noimageindex, unavailable_after: 2024-01-01"></meta>
       </>
     );
   }
