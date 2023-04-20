96fd4fe09dfbb82ceb2c10f7a0e9640bf3c64e65
James M <103403655+jfmow@users.noreply.github.com>
2023-04-20 18:18:06 +1200

    Update nav.jsx

--- app/nav.jsx
+++ app/nav.jsx
@@ -13,8 +13,8 @@ export default function Nav() {
             <div className={styles.navdiv}>
                 <h3><Link legacyBehavior passHref href="/"><a>James M</a></Link></h3>
                 <ul className={styles.navitems}>
-                <li className={styles.normalnavitem}><Link href="https://gallery.suddsy.dev/" target='_blank' aria-label='photo gallery'>Gallery</Link></li>
-                <li className={styles.normalnavitem}><Link href="https://news.suddsy.dev/" target='_blank' aria-label='news site'>News</Link></li>
+                <li className={styles.normalnavitem}><Link href="https://gallery.jamesmowat.com/" target='_blank' aria-label='photo gallery'>Gallery</Link></li>
+                <li className={styles.normalnavitem}><Link href="https://news.jamesmowat.com/" target='_blank' aria-label='news site'>News</Link></li>
                 <li className={styles.normalnavitem}><Link href="/projects" aria-label='my projects'>Projects</Link></li>
                 </ul>
             </div>
