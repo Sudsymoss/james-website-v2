dd1d793a75aab74380d9907c9fcbae1a6896bc09
sudsymoss <james1mowat@gmail.com>
2023-04-22 14:18:22 +1200

    cano

--- app/head.jsx
+++ app/head.jsx
@@ -4,7 +4,7 @@ export default function Head() {
       <title>Home</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
-      <link rel="canonical" href="https://suddsy.dev/" />
+      <link rel="canonical" href="https://jamesmowat.com/" />
       <meta name="description" content="A personal devloper profile. I wonder if the googlebot read this?"></meta>
       <meta name="robots" content="max-snippet:0, noarchive, notranslate, noimageindex, unavailable_after: 2024-01-01"></meta>
     </>
