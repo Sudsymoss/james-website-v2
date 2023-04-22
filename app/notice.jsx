2fd7177dc9fb7513f44665069f3084212eb58c58
sudsymoss <james1mowat@gmail.com>
2023-04-23 10:49:41 +1200

    style

--- app/notice.jsx
+++ app/notice.jsx
@@ -4,7 +4,7 @@ export default async function notice() {
         <div className={styles.main}>
             <div className={styles.notice} id="notice">
                 <div className={styles.msg}>
-                <h2>MAJOR SYSTEMS OUTAGE. Downtime: unknown at this time</h2>
+                <h2>MAJOR SYSTEMS OUTAGE! UPDATE: all systems back online</h2>
                 </div>
             </div>
         </div>
