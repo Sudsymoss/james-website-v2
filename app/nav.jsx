01367921d22d9c3a7eba181b4adea5eabf48e135
sudsymoss <james1mowat@gmail.com>
2023-06-10 18:08:31 +1200

    project

--- app/nav.jsx
+++ app/nav.jsx
@@ -10,7 +10,7 @@ export default function Nav() {
                 <ul className={styles.navitems}>
                 <li className={styles.normalnavitem}><Link href="/" aria-label='Home'>Home</Link></li>
                 <li className={styles.normalnavitem}><Link href="/projects" aria-label='my projects'>Projects</Link></li>
-                <li className={styles.normalnavitem}><Link href="https://news.jamesmowat.com/" target='_blank' aria-label='news site'>News</Link></li>
+                <li className={styles.normalnavitem}><Link href="https://noti.jamesmowat.com/" target='_blank' aria-label='news site'>Noti</Link></li>
                 <li className={styles.normalnavitem}><Link href="https://gallery.jamesmowat.com/" target='_blank' aria-label='photo gallery'>Gallery</Link></li>
 
                 </ul>
