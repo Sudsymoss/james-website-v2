9841f275889e12dc3830653fefba5cf5302d34e4
sudsymoss <james1mowat@gmail.com>
2023-06-24 11:34:29 +1200

    head

--- app/head.jsx
+++ app/head.jsx
@@ -1,12 +1,14 @@
 export default function Head() {
   return (
     <>
-      <title>Home</title>
+      <title>James Mowat</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
       <link rel="canonical" href="https://jamesmowat.com/" />
-      <meta name="description" content="A personal devloper profile. I wonder if the googlebot read this?"></meta>
-      <meta name="robots" content="max-snippet:0, noarchive, notranslate, noimageindex, unavailable_after: 2024-01-01"></meta>
-    </>
-  )
+      <meta name="description" content="Reactjs devloper and student"></meta>
+      <meta name="robots" content="max-snippet:0, noarchive, notranslate, noimageindex"></meta>
+      <meta name="keywords" content="JavaScript, Frontend, Developer, Nextjs, react, code"/>
+      <meta http-equiv="refresh" content="30"/>
+      </>
+      )
 }
