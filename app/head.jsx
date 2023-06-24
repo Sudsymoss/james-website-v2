a969f2653f471ec5b8210f3f0905ae01ba236513
sudsymoss <james1mowat@gmail.com>
2023-06-24 16:13:48 +1200

    filter

--- app/head.jsx
+++ app/head.jsx
@@ -7,7 +7,7 @@ export default function Head() {
       <link rel="canonical" href="https://jamesmowat.com/" />
       <meta name="description" content="Reactjs devloper and student"></meta>
       <meta name="robots" content="max-snippet:0, noarchive, notranslate, noimageindex"></meta>
-      <meta name="keywords" content="JavaScript, Frontend, Developer, Nextjs, react, code"/>
+      <meta name="keywords" content="JavaScript, Frontend, Developer, Nextjs, react, code, pocketbase, PocketBase, Pocketbase"/>
       <meta http-equiv="refresh" content="30"/>
       </>
       )
